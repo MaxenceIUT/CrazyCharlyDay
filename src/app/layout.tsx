1621f83782af9efc245833aafdf9ffc675aee330
MaxenceIUT <117634432+MaxenceIUT@users.noreply.github.com>
2023-02-16 17:05:45 +0100

    feat: added header, improved footer and updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,13 +21,13 @@ export default async function RootLayout({
   } = await supabase.auth.getSession()
 
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>
+      <body className="min-h-full flex flex-col">
         <ChakraClientProvider children={children} />
         <SupabaseListener accessToken={session?.access_token} />
       </body>
