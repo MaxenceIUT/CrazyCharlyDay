520d6471a99489bd8ff3ee6dc50a1a9636ca44ac
GaspardBBY <96123850+GaspardBBY@users.noreply.github.com>
2023-02-16 22:37:45 +0100

    fix: wront type

--- src/components/ArticlePanier.tsx
+++ src/components/ArticlePanier.tsx
@@ -1,78 +1,109 @@
-'use strict';
+"use strict";
 
-import { useRecoilState } from 'recoil';
-import { cartState } from '@/../src/atoms/cartState';
-import { Image, Popover, LinkBox, LinkOverlay, Text, Box, Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Heading, Stack, Wrap, WrapItem } from '@chakra-ui/react'
-import { useRouter } from 'next/navigation'
+import { useRecoilState } from "recoil";
+import { cartState } from "@/../src/atoms/cartState";
+import {
+  Image,
+  Popover,
+  LinkBox,
+  LinkOverlay,
+  Text,
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Heading,
+  Stack,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
 // on import l'icon basket
-import { FaShoppingBasket } from 'react-icons/fa'
+import { FaShoppingBasket } from "react-icons/fa";
 // on import icon like
-import { FaTrash } from 'react-icons/fa'
-import supabaseBrowser from '@/utils/supabase-browser';
+import { FaTrash } from "react-icons/fa";
+import supabaseBrowser from "@/utils/supabase-browser";
 
 type ArticleProps = {
-  id: number,
-  categorie: number,
-  nom: string,
-  quantite: number,  
-  prix: number,
-  image: string,
-  trashedFonction: any
-}
+  id: number;
+  categorie: number;
+  nom: string;
+  quantite: number;
+  prix: number;
+  image: string;
+  trashedFonction: any;
+};
 
 export default function Article(props: ArticleProps) {
-
   const router = useRouter();
 
   const [cart, setCart] = useRecoilState(cartState);
 
   const addToCart = async () => {
     if (props.categorie == 1) {
-      router.push('/articleInfo/' + props.id);
+      router.push("/articleInfo/" + props.id);
     } else {
       const jtwl = await supabaseBrowser.auth.getSession();
+      if (!jtwl.data.session) return;
       const id = jtwl.data.session.user.id;
-      let idCM = '';
-      const { data, error } = await supabaseBrowser.from('commande').select('*').eq('termine', false).eq('idUser', id);
+      let idCM;
+      const { data, error } = await supabaseBrowser
+        .from("commande")
+        .select("*")
+        .eq("termine", false)
+        .eq("idUser", id);
       if (!data || data.length == 0) {
-        const { data, error } = await supabaseBrowser.from('commande').insert({ idUser: id, termine: false }).select();
+        const { data, error } = await supabaseBrowser
+          .from("commande")
+          .insert({ idUser: id, termine: false })
+          .select();
+        if (!data) return;
         idCM = data[0].idCM;
       } else {
         idCM = data[0].idCM;
       }
       // setCart(prevState => [...cart, props]);
-      const dataPanier = await supabaseBrowser.from('panier').select().eq('id_commande', idCM).eq('id_produit', props.id);
-      
+      const dataPanier = await supabaseBrowser
+        .from("panier")
+        .select()
+        .eq("id_commande", idCM)
+        .eq("id_produit", props.id);
+
       if (dataPanier.data && dataPanier.data.length > 0) {
-        const { data, error } = await supabaseBrowser.from('panier').update({ quantite: dataPanier.data[0].quantite + 1 }).eq('id_commande', idCM).eq('id_produit', props.id);
+        const { data, error } = await supabaseBrowser
+          .from("panier")
+          .update({ quantite: dataPanier.data[0].quantite + 1 })
+          .eq("id_commande", idCM)
+          .eq("id_produit", props.id);
       } else {
-        const panier = await supabaseBrowser.from('panier').insert({ id_commande: idCM, id_produit: props.id, quantite: 1 }).select();
-        setCart(prevState => [...cart, panier.data[0]]);
-        
+        const panier = await supabaseBrowser
+          .from("panier")
+          .insert({ id_commande: idCM, id_produit: props.id, quantite: 1 })
+          .select();
+        setCart((prevState) => [...cart, panier.data[0]]);
       }
-
-
-
     }
-
-  }
+  };
 
   return (
     <div>
       <LinkBox>
-        <LinkOverlay >
-          <Card maxW='sm' className="hover:drop-shadow-lg">
+        <LinkOverlay>
+          <Card maxW="sm" className="hover:drop-shadow-lg">
             <CardBody>
               <div className="w-[100%] flex justify-center">
                 <Image
-                  src={'img/' + props.id + '.png'}
-                  alt='Green double couch with wooden legs'
-                  borderRadius='lg'
-                  height='250px'
+                  src={"img/" + props.id + ".png"}
+                  alt="Green double couch with wooden legs"
+                  borderRadius="lg"
+                  height="250px"
                 />
               </div>
-              <Stack mt='6' spacing='3'>
-                <Heading size='md' minH='3em'>
+              <Stack mt="6" spacing="3">
+                <Heading size="md" minH="3em">
                   {props.nom}
                 </Heading>
                 <div className="h-12">
@@ -80,7 +111,7 @@ export default function Article(props: ArticleProps) {
                     Il y en a {props.quantite} dans le panier
                   </Text>
                 </div>
-                <Text color='blue.600' fontSize='2xl'>
+                <Text color="blue.600" fontSize="2xl">
                   {props.prix}€
                 </Text>
               </Stack>
@@ -88,10 +119,14 @@ export default function Article(props: ArticleProps) {
             <Divider />
             <CardFooter className="items-center justify-around">
               <Text mr={2}>Ajouter directement au panier</Text>
-              <Button variant='solid' colorScheme='blue' onClick={addToCart}>
+              <Button variant="solid" colorScheme="blue" onClick={addToCart}>
                 <FaShoppingBasket />
               </Button>
-              <Button variant='solid' colorScheme='red' onClick={props.trashedFonction}>
+              <Button
+                variant="solid"
+                colorScheme="red"
+                onClick={props.trashedFonction}
+              >
                 <FaTrash />
               </Button>
             </CardFooter>
