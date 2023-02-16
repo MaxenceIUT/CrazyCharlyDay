cc5e27c50037503048c4df70f5aaa77c3df15025
MaxenceIUT <117634432+MaxenceIUT@users.noreply.github.com>
2023-02-16 17:05:19 +0100

    style: added some variants to theme colors

--- src/components/ChakraClientProvider.tsx
+++ src/components/ChakraClientProvider.tsx
@@ -8,8 +8,17 @@ import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
   colors: {
-    "primary-green": "#1c4c4b",
-    "secondary-gold": "#f5c548"
+    "primary-green": {
+      50: "#f0fff4",
+      900: "#1c4c4b",
+    },
+    "secondary-gold": {
+      50: "#fffaf0",
+      100: "#2c2c1b",
+      400: "#f6c648",
+      600: "#f5c548",
+      900: "#c69e2a",
+    }
   }
 });
 
