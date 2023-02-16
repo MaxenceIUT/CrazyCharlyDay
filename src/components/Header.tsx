68cedf87e5d6a516695a39a7b83540f88a4653b2
MaxenceIUT <117634432+MaxenceIUT@users.noreply.github.com>
2023-02-16 21:55:00 +0100

    fix: transformed Chakra links with NextJS links to improve app performance instead of doing a full page reload for each route

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,6 +23,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@chakra-ui/react';
+import NextLink from 'next/link';
 import Image from 'next/image';
 import { FaMoon, FaSun, FaChevronDown, FaChevronRight } from 'react-icons/fa';
 
@@ -34,6 +35,7 @@ import logo from '@/../public/logo.png';
 
 const NavLink = ({ children }: { children: ReactNode }) => (
   <Link
+    as={NextLink}
     px={2}
     py={1}
     rounded={'md'}
@@ -58,6 +60,7 @@ const DesktopNav = () => {
           <Popover trigger={'hover'} placement={'bottom-start'}>
             <PopoverTrigger>
               <Link
+                as={NextLink}
                 p={2}
                 href={navItem.href ?? '#'}
                 fontSize={'sm'}
@@ -96,6 +99,7 @@ const DesktopNav = () => {
 const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
   return (
     <Link
+      as={NextLink}
       href={href}
       role={'group'}
       display={'block'}
@@ -180,7 +184,7 @@ const MobileNavItem = ({ label, children, href }: NavItem) => {
           align={'start'}>
           {children &&
             children.map((child) => (
-              <Link key={child.label} py={2} href={child.href}>
+              <Link key={child.label} py={2} href={child.href} as={NextLink}>
                 {child.label}
               </Link>
             ))}
@@ -234,7 +238,9 @@ export default function Header() {
       <Box backgroundColor={bgColor} px={4}>
         <Flex h={24} alignItems={'center'} justifyContent={'space-between'}>
           <Flex className="w-full h-full">
-            <Image className='w-24 h-24 object-contain' src={logo} alt={'logo'} width={110} height={110} />
+            <Link href="/" as={NextLink}>
+              <Image className='w-24 h-24 object-contain' src={logo} alt={'logo'} width={110} height={110} />
+            </Link>
 
             <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
               <DesktopNav />
